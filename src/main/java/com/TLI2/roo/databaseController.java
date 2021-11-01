e70e811693bae8b4b94f793c97c5a3df1150ec0e
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2021-10-31 22:32:56 -0400

    adding database functionality for hosted environments

---
+++ src/main/java/com/TLI2/roo/databaseController.java
@@ -0,0 +1,19 @@
+//package com.TLI2.roo;
+//
+//import com.mysql.cj.log.Log;
+//
+//import java.sql.*;
+//import java.util.logging.Logger;
+//
+//public class databaseController {
+//
+//    private static Connection getRemoteConnection() {
+//        if (System.getenv("RDS_HOSTNAME") != null) {
+//            try {
+//
+//            }
+//            catch (ClassNotFoundException e)
+//        }
+//        return null;
+//    }
+//}
