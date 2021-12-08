e0c5b6a1c8b15c7cfc480493ff6f1b2c0b0cdd4c
annaa-cho <ac.anna.cho@gmail.com>
2021-12-08 03:25:32 -0500

    cut down methods in InitDatabase.java

--- src/main/java/database/InitDatabase.java
+++ src/main/java/database/InitDatabase.java
@@ -1,24 +1,25 @@
-package database;
+package Database;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class InitDatabase {
 
+    //  creates database 'auto_education', creates table 'car', populates table with data
     public static void main(Connection conn) throws SQLException, IOException {
         makeDatabase(conn);
         carTable(conn);
         writeToCarTable(conn, "data/sensoCarSet.csv");
     }
 
+    //  creates database 'auto_education' (unless it has already been created)
     private static void makeDatabase(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
 
@@ -28,6 +29,7 @@ public class InitDatabase {
         stmt.close();
     }
 
+    //  creates table 'car' in database 'auto_education'
     private static void carTable(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
 
@@ -64,6 +66,7 @@ public class InitDatabase {
         stmt.close();
     }
 
+    //  persists data from sensoCarSet.csv into db
     private static void writeToCarTable(Connection conn, String filePath) throws SQLException, IOException {
         String insert = "INSERT INTO car " +
                 "(car_id, make, model, carDescription, listPrice, year, kms, color, interior, interiorDescription, " +
@@ -71,166 +74,28 @@ public class InitDatabase {
                 "d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, image) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+        //  reads .csv
         PreparedStatement preparedStmt = conn.prepareStatement(insert);
         BufferedReader lineReader = new BufferedReader(new FileReader(filePath));
 
         int count = 0;
         int batchSize = 20;
         String lineText;
-
         lineReader.readLine();
 
         try {
+            //  loops through every line of the dataset
             while ((lineText = lineReader.readLine()) != null) {
                 List<String> data = new ArrayList<>();
-                int first_pos = 0;
-                boolean wrapped = false;
-
-                for (int last_pos = 0; last_pos < lineText.length(); last_pos++) {
-                    if (lineText.charAt(last_pos) == '\"') {
-                        wrapped = !wrapped;
-                    } else if (lineText.charAt(last_pos) == ',' && !wrapped) {
-                        data.add(lineText.substring(first_pos, last_pos));
-                        first_pos = last_pos + 1;
-                    }
-                }
-                data.add(lineText.substring(first_pos));
-
-                String car_id = data.get(0);
-                String make = data.get(1);
-                String model = data.get(2);
-                String carDescription = data.get(3);
-                String listPrice = data.get(4);
-                String year = data.get(5);
-                String kms = data.get(6);
-                String color = data.get(7);
-                String interior = data.get(8);
-                String interiorDescription = data.get(9);
-                String engine = data.get(10);
-                String engineDescription = data.get(11);
-                String performancePackage = data.get(12);
-                String performancePackageDescription = data.get(13);
-                String carCondition = data.get(14);
-                String d1 = data.get(15);
-                String d2 = data.get(16);
-                String d3 = data.get(17);
-                String d4 = data.get(18);
-                String d5 = data.get(19);
-                String d6 = data.get(20);
-                String d7 = data.get(21);
-                String d8 = data.get(22);
-                String d9 = data.get(23);
-                String d10 = data.get(24);
-                String image = data.get(25);
-
-                int int_car_id = Integer.parseInt(car_id);
-                preparedStmt.setInt(1, int_car_id);
-
-                preparedStmt.setString(2, make);
-
-                preparedStmt.setString(3, model);
-
-                if (carDescription.isEmpty() || carDescription.charAt(0) != '"') {
-                    preparedStmt.setString(4, carDescription);
-                } else {
-                    String str = carDescription.substring(1, carDescription.length() - 1);
-                    preparedStmt.setString(4, str);
-                }
-
-                float float_listPrice = Float.parseFloat(listPrice);
-                preparedStmt.setFloat(5, float_listPrice);
-
-                int int_year = Integer.parseInt(year);
-                preparedStmt.setInt(6, int_year);
-
-                int int_kms = Integer.parseInt(kms);
-                preparedStmt.setInt(7, int_kms);
-
-                preparedStmt.setString(8, color);
-
-                if (interior.isEmpty() || interior.charAt(0) != '"') {
-                    preparedStmt.setString(9, interior);
-                } else {
-                    String str = interior.substring(1, interior.length() - 1);
-                    preparedStmt.setString(9, str);
-                }
-
-                if (interiorDescription.isEmpty() || interiorDescription.charAt(0) != '"') {
-                    preparedStmt.setString(10, interiorDescription);
-                } else {
-                    String str = interiorDescription.substring(1, interiorDescription.length() - 1);
-                    preparedStmt.setString(10, str);
-                }
-
-                if (engine.isEmpty() || engine.charAt(0) != '"') {
-                    preparedStmt.setString(11, engine);
-                } else {
-                    String str = engine.substring(1, engine.length() - 1);
-                    preparedStmt.setString(11, str);
-                }
-
-                if (engineDescription.isEmpty() || engineDescription.charAt(0) != '"') {
-                    preparedStmt.setString(12, engineDescription);
-                } else {
-                    String str = engineDescription.substring(1, engineDescription.length() - 1);
-                    preparedStmt.setString(12, str);
-                }
-
-                if (performancePackage.isEmpty() || performancePackage.charAt(0) != '"') {
-                    preparedStmt.setString(13, performancePackage);
-                } else {
-                    String str = performancePackage.substring(1, performancePackage.length() - 1);
-                    preparedStmt.setString(13, str);
-                }
-
-                if (performancePackageDescription.isEmpty() || performancePackageDescription.charAt(0) != '"') {
-                    preparedStmt.setString(14, performancePackageDescription);
-                } else {
-                    String str = performancePackageDescription.substring(1, performancePackageDescription.length() - 1);
-                    preparedStmt.setString(14, str);
-                }
-
-                preparedStmt.setString(15, carCondition);
-
-                float float_d1 = Float.parseFloat(d1);
-                preparedStmt.setFloat(16, float_d1);
-
-                float float_d2 = Float.parseFloat(d2);
-                preparedStmt.setFloat(17, float_d2);
-
-                float float_d3 = Float.parseFloat(d3);
-                preparedStmt.setFloat(18, float_d3);
-
-                float float_d4 = Float.parseFloat(d4);
-                preparedStmt.setFloat(19, float_d4);
-
-                float float_d5 = Float.parseFloat(d5);
-                preparedStmt.setFloat(20, float_d5);
-
-                float float_d6 = Float.parseFloat(d6);
-                preparedStmt.setFloat(21, float_d6);
-
-                float float_d7 = Float.parseFloat(d7);
-                preparedStmt.setFloat(22, float_d7);
-
-                float float_d8 = Float.parseFloat(d8);
-                preparedStmt.setFloat(23, float_d8);
-
-                float float_d9 = Float.parseFloat(d9);
-                preparedStmt.setFloat(24, float_d9);
-
-                float float_d10 = Float.parseFloat(d10);
-                preparedStmt.setFloat(25, float_d10);
-
-                preparedStmt.setString(26, image);
-
+                checkWrappedText(lineText, data);   // checks if an entry has commas encased within quotations
+                Map<String, String> cols = getData(data);   //  gets data from csv
+                parseData(preparedStmt, cols);  // ensures proper format for each entry
                 preparedStmt.addBatch();
                 count++;
 
                 if (count % batchSize == 0) {
                     preparedStmt.executeBatch();
                 }
-
             }
             lineReader.close();
             preparedStmt.executeBatch();
@@ -239,7 +104,93 @@ public class InitDatabase {
         } catch (IOException | SQLException e) {
             System.out.println(e);
         }
+    }
 
+    //  checks if an entry has commas within an entry itself
+    private static void checkWrappedText(String lineText, List<String> data) {
+        int first_pos = 0;
+        boolean wrapped = false;
+
+        for (int last_pos = 0; last_pos < lineText.length(); last_pos++) {
+            if (lineText.charAt(last_pos) == '\"') {
+                wrapped = !wrapped;
+            } else if (lineText.charAt(last_pos) == ',' && !wrapped) {
+                data.add(lineText.substring(first_pos, last_pos));
+                first_pos = last_pos + 1;
+            }
+        }
+        data.add(lineText.substring(first_pos));
+    }
+
+    //  puts data into a hashmap (key is the column name, value is the data itself)
+    private static Map<String, String> getData(List<String> data) {
+        Map<String, String> cols = new HashMap<>();
+        cols.put("car_id", data.get(0));
+        cols.put("make", data.get(1));
+        cols.put("model", data.get(2));
+        cols.put("carDescription", data.get(3));
+        cols.put("listPrice", data.get(4));
+        cols.put("year", data.get(5));
+        cols.put("kms", data.get(6));
+        cols.put("color", data.get(7));
+        cols.put("interior", data.get(8));
+        cols.put("interiorDescription", data.get(9));
+        cols.put("engine", data.get(10));
+        cols.put("engineDescription", data.get(11));
+        cols.put("performancePackage", data.get(12));
+        cols.put("performancePackageDescription", data.get(13));
+        cols.put("carCondition", data.get(14));
+        cols.put("d1", data.get(15));
+        cols.put("d2", data.get(16));
+        cols.put("d3", data.get(17));
+        cols.put("d4", data.get(18));
+        cols.put("d5", data.get(19));
+        cols.put("d6", data.get(20));
+        cols.put("d7", data.get(21));
+        cols.put("d8", data.get(22));
+        cols.put("d9", data.get(23));
+        cols.put("d10", data.get(24));
+        cols.put("image", data.get(25));
+        return cols;
+    }
+
+    //  removes quotation marks (applies to entries that have commas within themselves)
+    private static void removeQuotes(PreparedStatement preparedStmt, String col, int index) throws SQLException {
+        if (col.isEmpty() || col.charAt(0) != '"') {
+            preparedStmt.setString(index, col);
+        } else {
+            String str = col.substring(1, col.length() - 1);
+            preparedStmt.setString(index, str);
+        }
+    }
+
+    //  makes sure data is of the proper type and contains no quotes
+    private static void parseData(PreparedStatement preparedStmt, Map<String, String> cols) throws SQLException {
+        preparedStmt.setInt(1, Integer.parseInt(cols.get("car_id")));
+        preparedStmt.setString(2, cols.get("make"));
+        preparedStmt.setString(3, cols.get("model"));
+        removeQuotes(preparedStmt, cols.get("carDescription"), 4);
+        preparedStmt.setFloat(5, Float.parseFloat(cols.get("listPrice")));
+        preparedStmt.setInt(6, Integer.parseInt(cols.get("year")));
+        preparedStmt.setInt(7, Integer.parseInt(cols.get("kms")));
+        preparedStmt.setString(8, cols.get("color"));
+        removeQuotes(preparedStmt, cols.get("interior"), 9);
+        removeQuotes(preparedStmt, cols.get("interiorDescription"), 10);
+        removeQuotes(preparedStmt, cols.get("engine"), 11);
+        removeQuotes(preparedStmt, cols.get("engineDescription"), 12);
+        removeQuotes(preparedStmt, cols.get("performancePackage"), 13);
+        removeQuotes(preparedStmt, cols.get("performancePackageDescription"), 14);
+        preparedStmt.setString(15, cols.get("carCondition"));
+        preparedStmt.setFloat(16, Float.parseFloat(cols.get("d1")));
+        preparedStmt.setFloat(17, Float.parseFloat(cols.get("d2")));
+        preparedStmt.setFloat(18, Float.parseFloat(cols.get("d3")));
+        preparedStmt.setFloat(19, Float.parseFloat(cols.get("d4")));
+        preparedStmt.setFloat(20, Float.parseFloat(cols.get("d5")));
+        preparedStmt.setFloat(21, Float.parseFloat(cols.get("d6")));
+        preparedStmt.setFloat(22, Float.parseFloat(cols.get("d7")));
+        preparedStmt.setFloat(23, Float.parseFloat(cols.get("d8")));
+        preparedStmt.setFloat(24, Float.parseFloat(cols.get("d9")));
+        preparedStmt.setFloat(25, Float.parseFloat(cols.get("d10")));
+        preparedStmt.setString(26, cols.get("image"));
     }
 }
-
