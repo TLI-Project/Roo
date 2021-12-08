c9bc282fcd4ce1705a5ce891576b973a1b3fe1bf
Joanne Pan <71951015+jojoonthat@users.noreply.github.com>
2021-12-08 16:45:07 -0500

    fixed db url

--- src/main/java/usecases/SensoReadyInfo.java
+++ src/main/java/usecases/SensoReadyInfo.java
@@ -9,15 +9,17 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static constants.DatabaseConstants.DB_URL;
+
 public class SensoReadyInfo {
     public static String userCarLoanRequest(Person inputData) throws IOException, InterruptedException {
 
         String loanInputJson = ApiInputAdapter.makeLoanInputJSON(inputData);
 
         var loanRequest = HttpRequest.newBuilder()
-                .uri(URI.create(System.getenv("SENSO_URL")))
+                .uri(URI.create("https://auto-loan-api.senso.ai/rate"))
                 .header("Content-Type", "application/json")
-                .header("x-api-key", System.getenv("SENSO_KEY"))
+                .header("x-api-key", "AIzaSyCD_-qCdXqrvWGHN1tpe2PH6Rf8zpnTdXs")
                 .POST(HttpRequest.BodyPublishers.ofString(loanInputJson))
                 .build();
 
