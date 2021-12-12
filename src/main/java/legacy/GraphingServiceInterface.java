5845eb15a0021bae035cd90de6ef94eede046163
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2021-12-12 16:18:53 -0500

    Removing legacy code.

--- src/main/java/legacy/GraphingServiceInterface.java
+++
@@ -1,8 +0,0 @@
-//package interfaces;
-//
-//import entities.GraphingData;
-//
-//public interface GraphingServiceInterface {
-//    public GraphingData saveUpdatedPerson(GraphingData person);
-//    public GraphingData findPersonById(Integer id);
-//}
